084eea5a87d4124824873c88dfc12fedaf2804f6
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-10-05 07:15:11 +0200

    Add files via upload

---
+++ TEst.py
@@ -0,0 +1,54 @@
+#zad 1
+# a=int(input("podaj liczbe a:"))
+# if a%3==0:
+#     print("Tak")
+# else:
+#     print("Nie")
+#zad 2
+# a=int(input("podaj liczbe a:"))
+# if a>=100 and a<1000 and a%17==0:
+#     print("Tak")
+# else:
+#     print("Nie")
+#zad 3
+# wiek=int(input("ile masz lat?:"))
+# if wiek>=18:
+#     print("Tak")
+# else:
+#     print("Nie")
+#zad 4
+# waga=int(input("ile ton?:"))
+# if waga<20:
+#     print("Tak")
+# else:
+#     print("Nie")
+#zad 5
+# a=int(input("podaj liczbe a:"))
+# b=int(input("podaj liczbe b:"))
+# c=int(input("podaj liczbe c:"))
+# if c>a and c<b:
+#     print("Tak")
+# else:
+#     print("Nie")
+#zad 6
+# p=int(input("podaj liczbe pierwszą p:"))
+# a=int(input("podaj liczbe a:"))
+# if (a**p-a)%p==0:
+#     print("Tak")
+# else:
+#     print("Nie")
+#zad 7
+# s=float(input("podaj wartość skoku:"))
+# p=float(input("wprowadź wartość startu:"))
+# k=float(input("wprowadź wartość końca:"))
+# if s*3+p>=k:
+#     print("Tak")
+#     print("pokonała dystansu:")
+#     print(s*3)
+# else:
+#     print("Nie")
+#     print("pokonała dystansu:")
+#     print(s*3)
+#     print("zabrakło jej:")
+#     print(k-(s*3+p))
+
