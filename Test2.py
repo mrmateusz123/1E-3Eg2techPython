8dee298646921c9bcc332364ee3ea10eb4b33f8f
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-10-10 12:14:54 +0000

    nothing

---
+++ Test2.py
@@ -0,0 +1,28 @@
+# p=int(input("podaj liczbe p:"))
+# q=int(input("podaj liczbe q:"))
+# if (1.3*p>q):
+#     print("tak")
+# else:
+#     print("nie")
+#test
+# print(list(range(10)))
+# print(list(range(5)))
+# print(list(range(2,10)))
+# print(list(range(12,10,-1)))
+# print(list(range(1,10,2)))
+# for i in range(0,10,2):
+#   print(i, end="")
+# L = [0,1,2,3,4,5,6]
+# print(L[0:6:2])
+#zad 1
+# n=int(input())
+# for i in range(n):
+#   print(i**3+3,end=" ")
+# for i in range(10,22):
+#   print(i,end=" ")
+# for i in range(15,32,2):
+#   print(i,end=" ")
+# for i in range(99,9,-1):
+#   print(i,end=" ")
+  for i in range(,,):
+  print(i,end=" ")
