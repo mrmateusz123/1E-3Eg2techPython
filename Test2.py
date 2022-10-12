b842830a3b84ed6075b39f01dade37c6cb4ed88b
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-10-12 06:31:56 +0000

    Test

--- Test2.py
+++ Test2.py
@@ -17,12 +17,43 @@
 #zad 1
 # n=int(input())
 # for i in range(n):
-#   print(i**3+3,end=" ")
+#   print(i**3+3,end=",")
 # for i in range(10,22):
-#   print(i,end=" ")
+#   print(i,end=",")
 # for i in range(15,32,2):
-#   print(i,end=" ")
+#   print(i,end=",")
 # for i in range(99,9,-1):
-#   print(i,end=" ")
-  for i in range(,,):
-  print(i,end=" ")
+#   print(i,end=",")
+# for i in range(10,100,1):
+  # print(109-i,end=",")
+# for i in range(100,1000,20):
+#   print(i,end=",")
+# for i in range(5,50,1): print(i*20,end=",")
+#zad 2
+# for i in range(105,1000,15): print(i,end=",")
+#zad 3
+# n=int(input())
+# for i in range(1,n+1):
+#   if n%i==0:
+#     print(i,end=",")
+#zad 4
+# s=0
+# for i in range(10,100):
+#   s=s+i
+# print(s)
+#zad 5
+# n=int(input())
+# s=n*(n+1)//2
+# for i in range(n-1):
+#   x=int(input())
+#   s=s-x
+# print("niepodałeś liczby:",s)
+#zad 6
+# n=int(input("podaj:"))
+# s=0
+# t=1
+# print(s,end=",")
+# print(t,end=",")
+# for i in range(n-2):
+#   s, t=t , s+t
+#   print(t,end=",")
