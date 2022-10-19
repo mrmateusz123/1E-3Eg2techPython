d68c069da239ac23d75791837984392454a56d5a
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-10-19 06:08:42 +0000

    test

---
+++ Test3a.py
@@ -0,0 +1,12 @@
+# zad 1
+# n=int(input())
+# for i in range(n):
+#   print("x-|")
+# zad 2
+# n=int(input())
+# for i in range(1,n+1):
+#  print("*"*i,end="")
+#  if i%2:
+#    print("||",end="")
+#  else:
+#    print("--",end="")
---
+++ Test5.py
@@ -0,0 +1,31 @@
+# Pętla for liczb trzycyforowych podzielnych przez 13 (2 sposoby)
+
+# for i in range(104,1000,13):
+#     print(i, end=" ")
+
+# for i in range(100,1000):
+#     if i % 13 == 0:
+#         print(i, end=" ")
+
+# Pętla for liczb dwucyfrowych parzystych (3 sposoby)
+
+# for i in range(10,100,2):
+#     print(i, end=" ")
+
+# for i in range(10,100):
+#     if i % 2 == 0:
+#         print(i, end=" ")
+
+# for i in range(5, 50):
+#     print(i*2, end=" ")
+        
+# Pętla for potęg cyfr: WY: 0, 1, 4, 9, 16 .. 81 (2 sposoby printa)
+# for i in range(0,10):
+#   print(i*i, end=" ")
+# for t in range(0,10):
+#   print(t**2, end=" ")
+#   for e in range(10):
+#     print(f"i^2={e**2}")
+# for i in range(10):
+#   print(i, "^ 2=",i**2)
+
