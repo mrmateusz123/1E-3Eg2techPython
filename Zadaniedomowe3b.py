32940209f50afde5d480714aa1ffc32edfc43345
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-10-23 15:44:41 +0000

    Nothing

---
+++ Zadaniedomowe3b.py
@@ -0,0 +1,33 @@
+# Zadanie 1
+# for i in range(1,31):
+#   print(i)
+# Zadanie 2
+# n=int(input("Podaj ile liczb nieparzystych:"))
+# for i in range(1,n*2+1,2):
+#   print(i*i)
+# Zadanie 3
+# for i in range(1137,10000,379):
+#   print(i)
+# Zadanie 4
+# for i in range(100,1000):
+#   if (i%5==0 or i%6==0 or i%11==0):
+#     print(i)
+# Zadanie 5
+# n=int(input("ile liczb:"))
+# w=0
+# for i in range(0,n):
+#   t=int(input("Liczba:"))
+#   w=w+t
+# print(w)
+# Zadanie 6
+# l=int(input())
+# k=0
+# for i in range(0,l*2+1,2):
+#   k=k+i
+# print(k)
+# Zadanie 7
+# m=0
+# for i in range(11,100,2):
+#   m=m+i
+# print(m)
+# Zadanie 8
