0ece502b10f7a4bc6f20b25bac349ed822e17833
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-10-25 23:34:37 +0200

    Create Zadanie3b.py

---
+++ Zadanie3b.py
@@ -0,0 +1,64 @@
+# Zadanie 1
+# for i in range(1,31):
+#   print(i)
+# Zadanie 2
+# n=int(input("Podaj ile liczb nieparzystych:"))
+# for i in range(1,n*2+1,2):
+#   print(i*i)
+# Zadanie 3
+# for i in range(1137,10000,379):
+#   print(i)
+# Zadanie 4
+# for i in range(100,1000):
+#   if (i%5==0 or i%6==0 or i%11==0):
+#     print(i)
+# Zadanie 5
+# n=int(input("ile liczb:"))
+# w=0
+# for i in range(0,n):
+#   t=int(input("Liczba:"))
+#   w=w+t
+# print(w)
+# Zadanie 6
+# l=int(input())
+# k=0
+# for i in range(0,l*2+1,2):
+#   k=k+i
+# print(k)
+# Zadanie 7
+# m=0
+# for i in range(11,100,2):
+#   m=m+i
+# print(m)
+# Zadanie 8
+# for i in range(0,n+1)
+# zadanie 9
+# n = int(input("Podaj ile liczb:"))
+# w = 0
+# t=-79
+# for i in range(21,n*21+1+n*100,100):
+#   w = w + i
+#   t=t+100
+# print(w-t)
+# Zadanie 10
+# w2=0
+# w=0
+# t=0
+# t2=0
+# o=10
+# for i in range(0,1000):
+#   o=10
+#   if(i>100):
+#     o=o+990
+#   elif(i>10):
+#     o=o+90
+#   i=i-w
+#   t=i*i-(o*t2)
+#   if(t<0):
+#     w=0
+#   if(t>0):
+#     if(i==t):
+#       print(i)
+#     elif(t>=10):
+#       t2=t2+1
+#       w=w+1
