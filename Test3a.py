20750a999dcebfb2ffbe2de00b48d75181f1be4a
mrmateusz123 <114559755+mrmateusz123@users.noreply.github.com>
2022-11-02 07:33:00 +0000

    test

--- Test3a.py
+++ Test3a.py
@@ -18,7 +18,84 @@
 #  else:
 #   print("*","|"*i,"*", end="")
 # Pre
-for i in range(1,11):
-  for j in range(1,11):
-    print(i*j,end="\t")
+# for i in range(1,11):
+#   for j in range(1,11):
+#     print(i*j,end="\t")
+#   print()
+n=int(input("test:"))
+# for i in range(n+1):
+#   print("*"*i,end="")
+#   print(" ")
+# for i in range(n+1):
+#   print("*"*(n-i-1),end="")
+#   print(" ")
+# for i2 in range(n):
+#   for j in range(i2+1):
+#     print("*",end="")
+#   print()
+# print()
+# for i3 in range(n):
+#   for j3 in range(n-i3):
+#     print("*",end="")
+#   print()
+# print()
+# print()
+# for i4 in range(n):
+#   for j4 in range(n-i4-1):
+#     print(" ",end="")
+#   for k4 in range(n-i4-1,n):
+#     print("*",end="")
+#   print()
+# print()
+# for i5 in range(n):
+#   for j5 in range(i5):
+#     print(" ",end="")
+#   for k5 in range(i5,n):
+#     print("*",end="")
+#   print()
+# print()
+for i in range(n):
+  for j in range(n):
+    if i>=j:
+      print("*",end="")
+    else:
+      print(" ",end="")
+  print()
+print()
+for i in range(n):
+  for j in range(n):
+    if j>=i:
+      print("*",end="")
+    else:
+      print(" ",end="")
+  print()
+print()
+for i in range(n):
+  for j in range(n):
+    if j==i:
+      print("*",end="")
+    else:
+      print(" ",end="")
+  print()
+print()
+for i in range(n):
+  for j in range(n):
+    if i+j==n-1:
+      print("*",end="")
+    else:
+      print(" ",end="")
+  print()  
+for i in range(n):
+  for j in range(n):
+    if i>j:
+      print(" ",end="")
+    else:
+      print("*",end="")
+  print()
+for i in range(n):
+  for j in range(n):
+    if i<=j:
+      print("*",end="")
+    else:
+      print(" ",end="")
   print()
