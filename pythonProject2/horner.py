bae8f62657cf60d0bc7ee458a0da4155a8114c30
test <114559755+mrmateusz123@users.noreply.github.com>
2024-11-13 15:07:13 +0100

    Update horner.py

--- pythonProject2/horner.py
+++ pythonProject2/horner.py
@@ -1,20 +1,21 @@
 def horner(T,x,n):
-    y = T[0]
+    y = T[n]
     pot = 1
-    for i in range(1,n):
+    for i in range(n,0,-1):
         pot = pot * x
-        y = y + T[i] * pot
+        y += T[i-1] * pot
     return y
-def honer2(T,x,n):
-    y =  T[n-1]
-    for i in range(n-1,0,-1):
-        y = x * y + T[i]
-    return y
+def honer2(T,x):
+    n = len(T)-1
+    if(n==0): return T[0]
+    return x*honer2(T[1:],x)+T[0]
+
 def z1():
     t = [int(i) for i in input().split()]
-    x = int(input())
-    print(honer2(t,x,len(t)))
-    t.reverse()
-    print(horner(t,x,len(t)))
-
-z1()
+    x = t.pop(0)
+    print(horner(t,x,len(t)-1))
+def z2():
+    t = [int(i) for i in input().split()]
+    x = t.pop(0)
+    print(honer2(t,x))
+z2()
