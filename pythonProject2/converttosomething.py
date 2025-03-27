d3b4132e2d7f3ebbad301b9d4d42183d0ee89d3f
mr. mateusz <114559755+mrmateusz123@users.noreply.github.com>
2025-03-27 07:55:42 +0100

    Update converttosomething.py

--- pythonProject2/converttosomething.py
+++ pythonProject2/converttosomething.py
@@ -1,5 +1,5 @@
 def z1():
-    bin = list(input().split(','))
+    bin = list(input("licz: ").split(','))
     wyn = 0
     pot = 1
     tak = 1
@@ -9,7 +9,7 @@ def z1():
         for i in cz:
             if (i == "1"):
                 wyn += 2 ** (pot - 1)
-            pot += tak
+            pot+=tak
         tak = -1
         pot = 0
     print(wyn)
@@ -18,6 +18,22 @@ def z1():
 def z2():
     dec = list(input().split(","))
     bin = ""
+    cz1 = int(dec[0])
+    b2in = 0
+    while cz1>0:
+        bin+=str(cz1%2)
+        cz1=cz1//2
+    bin = bin[::-1]
+    print(bin)
+    pot = -1
+    print("0."+dec[1])
+    cz2 = int("0."+dec[1])
+    while b2in != cz2:
+        if b2in + 2**pot < cz2:
+            b2in += 2**pot
+        pot-=1
+        print(b2in,int("0,",dec[1]))
+    print(bin)
 
 
 def zad():
